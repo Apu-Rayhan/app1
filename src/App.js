882a839e7b4a875724ec3c38a80f6ed605f2905d
apu <apuhot168@gmail.com>
2022-06-08 02:54:24 +0600

    kkd

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <h1>hello react js</h1>
+    <h2>kdkfdl</h2>
+     
+        
+    </div>
+  );
+}
+
+export default App;
