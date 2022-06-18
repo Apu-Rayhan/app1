426f720dd2940179de863b2100a5b45e76cafc62
apu <apuhot168@gmail.com>
2022-06-19 02:21:00 +0600

    gg

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    
+
+    <>
+    <div className='home'>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur debitis, sit sint dolor doloremque voluptate perferendis doloribus voluptates? Sed autem nulla cupiditate nesciunt deleniti odio quis quod temporibus assumenda velit!</p>
+    </div>
+    </>
+    
+    
+    
+    
   )
 }
 
