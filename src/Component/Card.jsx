235a6922c3b27ec79764cf4021ef5988241638b4
apu <apuhot168@gmail.com>
2022-06-19 02:41:03 +0600

    tt

---
+++ src/Component/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          debitis, sit sint dolor doloremque voluptate perferendis doloribus
+          voluptates? Sed autem nulla cupiditate nesciunt deleniti odio quis
+          quod temporibus assumenda velit!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from "react";
+import Card from "../Component/Card";
 
 const Home = () => {
   return (
-    
-
     <>
-    <div className='home'>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur debitis, sit sint dolor doloremque voluptate perferendis doloribus voluptates? Sed autem nulla cupiditate nesciunt deleniti odio quis quod temporibus assumenda velit!</p>
-    </div>
+      <div className="home">
+      <Card/>
+        
+      </div>
     </>
-    
-    
-    
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
